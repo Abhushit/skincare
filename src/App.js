531d78052361f988dafc58443c98e5c771550ccc
Abhushit Chaudhary <chaudhary.abhushit@gmail.com>
2023-03-15 21:49:28 +0545

    project inititalized

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
+import React from "react";
+import { CameraFill } from "react-bootstrap-icons";
+import Webcam from "react-webcam";
 import './App.css';
 
+const videoConstraints = {
+  width: 700,
+  height: 800,
+  facingMode: "user",
+};
+
+const svgIcon = () => (
+  <svg
+    width="100%"
+    height="100%"
+    className="svg"
+    // viewBox="0 0 175 200"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+  >
+    <defs>
+      <mask id="overlay-mask" x="0" y="0" width="100%" height="100%">
+        <rect x="0" y="0" width="100%" height="100%" fill="#fff" />
+        <circle cx="50%" cy="50%" r="185" />
+      </mask>
+    </defs>
+    <rect
+      x="0"
+      y="0"
+      width="100%"
+      height="100%"
+      mask="url(#overlay-mask)"
+      fillOpacity="0.4"
+    />
+  </svg>
+);
+
 function App() {
+  const webcamRef = React.useRef(null);
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+  }, [webcamRef]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="webcam-container">
+        <Webcam
+          audio={false}
+          // height={800}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          // width={700}
+          // videoConstraints={videoConstraints}
+        />
+        <div className="overlay-container">{svgIcon()}</div>
+        <div className="text-white">
+          <h2>Please stay in the mask</h2>
+          <p>Press camera to click photo</p>
+        </div>
+        <button onClick={capture} className="btn_click">
+          <CameraFill color="white" size={30} />
+        </button>
+      </div>
+    </>
   );
 }
 
